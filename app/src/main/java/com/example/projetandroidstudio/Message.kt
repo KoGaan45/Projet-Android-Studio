22b5e8bca3e51a851a368ddae6f3e1553218a981
Axel <a.malsagne@outlook.fr>
2022-12-18 11:28:10 +0100

    Doublon chat gérer

--- app/src/main/java/com/example/projetandroidstudio/Message.kt
+++ app/src/main/java/com/example/projetandroidstudio/Message.kt
@@ -3,4 +3,13 @@ package com.example.projetandroidstudio
 import java.util.Date
 
 class Message(val id: Int, val auteur: String, val contenu: String, val date: Date?) {
+
+    // La méthode contains d'une arrayList fait appel à equals la surcharger permet de comparer les messages
+    override fun equals(other: Any?): Boolean {
+        super.equals(other)
+
+        if (other is Message && other.id == this.id) return true
+
+        return false
+    }
 }
